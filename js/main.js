4c20b00d0101dc8b57ce30fe545e2a5ba8e9a253
[KSP] <[kspavlov@gmail.com]>
2018-01-29 14:40:40 +0200

    add tabs js html and css

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-
 // toggle dropdown menu
 $(function() {
     const dropdown = (() => {
-    const openClose = (object) => {
-        $(object).find('.dropdown-menu').toggleClass('hidden');
-        $(object).closest('.dropdown').toggleClass('is-active');
-    };
+        const openClose = (object) => {
+            $(object).find('.dropdown-menu').toggleClass('hidden');
+            $(object).closest('.dropdown').toggleClass('is-active');
+        };
 
-    const changeValue = () => {
+        const changeValue = () => {
 
-    };
+        };
 
-    const changeOptionValue = () => {
+        const changeOptionValue = () => {
 
-    };
+        };
 
         return {
             openClose,
@@ -24,13 +23,12 @@ $(function() {
         dropdown.openClose(this);
     });
 
-    $(document).on('click', function (e) {
+    $(document).on('click', function(e) {
         if ($(e.target).closest(".dropdown.is-active").length === 0) {
-             $(".dropdown-menu").addClass('hidden');
+            $(".dropdown-menu").addClass('hidden');
         }
     });
 });
-
 
 // mark dropdown item as active
 $(function() {
@@ -39,5 +37,21 @@ $(function() {
         $(this).parents('.dropdown').find('.value-of-select').text(valueOfOption);
         $(this).parent().find('.dropdown-item').removeClass('is-active');
         $(this).addClass('is-active');
+    });
+});
+
+// select a tab and show list
+$(function() {
+    $('.tabs.is-boxed li').on('click', function() {
+        $(this).closest('.tabs.is-boxed').find('li').removeClass('is-active');
+        $(this).addClass ('is-active');
+    });
+});
+
+$(function() {
+    $('.tabs.is-boxed li').on('click', function() {
+        var panelId = $(this).attr('data-panelid');
+        $(this).closest('.tab-menu').find('div.tab-pane').addClass('hidden');
+        $(panelId).removeClass('hidden');
     });
 });
