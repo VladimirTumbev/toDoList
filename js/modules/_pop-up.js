13db60b8372d1a94526a29a8afb48ef74b2a2601
Vladimir Tumbev <vladimirgtumbev@gmail.com>
2018-01-30 17:07:55 +0200

    added a seperate html for the header

--- js/modules/_pop-up.js
+++ js/modules/_pop-up.js
@@ -1,10 +1,36 @@
-$(function (){
-    $('.pop-up-open').on('click', function(){
+$(function () {
+    $('.pop-up-open').on('click', function () {
         var popUpDiv = $(this).data('target');
         $(popUpDiv).toggleClass('show');
     });
-    $('.pop-up-close').on('click', function(){
+    $('.pop-up-close').on('click', function () {
         var popUpDiv = $(this).data('target');
         $(popUpDiv).toggleClass('show');
     });
+
+    var isOut = false;
+
+    $('.pop-up-dialog').mouseover(function () {
+        isOut = false;
+    });
+
+    $('.pop-up-dialog').mouseout(function () {
+        isOut = true;
+    });
+
+    $(document).on('keyup', function (e) {
+        if (e.keyCode === 27) {
+            if ($('#addTaskPopUpNav').hasClass('show')) {
+                $('#addTaskPopUpNav').removeClass('show');
+            }
+        }
+    })
+
+    $(document).on('click', function () {
+        if (isOut) {
+            $('#addTaskPopUpNav').removeClass('show');
+            isOut = false;
+        }
+    })
+
 });
