410766f2c12b18aa48b40d06e04a973300ba13bd
Vladimir Tumbev <vladimirgtumbev@gmail.com>
2018-02-01 19:15:45 +0200

    added pop-up business logic functionality

---
+++ js/main.js
@@ -0,0 +1,92 @@
+(function () {
+    var taskToAdd = {
+        comments: [],
+        init: function () {
+            this.cacheDom();
+            this.bindEvents();
+        },
+        cacheDom: function () {
+            this.$name = $('#new-task-name');
+            this.$projects = $('#add-to-project');
+            this.$priority = $('#priority');
+            this.$dueDate = $('#due-date');
+            this.$reminder = $('#reminder');
+            this.$description = $('#description');
+            this.$newComment = $('#comment');
+            this.$createButton = $('#createTask');
+            this.$saveCommentBtn = $('#saveComment');
+        },
+        bindEvents: function () {
+            this.$createButton.on('click', this.validateAndCreate.bind(this));
+            this.$saveCommentBtn.on('click', this.addComment.bind(this));
+        },
+
+        getValues: function () {
+            return {
+                taskName: this.$name.val() || 'N/A',
+                projects: this.$projects.val().split(', ') || 'N/A',
+                priority: this.$priority.val() || 1,
+                dueDate: this.$dueDate.val(),
+                description: this.$description.val() || 'N/A',
+                reminder: this.$reminder.val(),
+                newComment: this.$newComment.val() || '',
+            }
+        },
+
+        addComment: function () {
+            var values = this.getValues();
+            this.comments.push(values.newComment)
+            values.newComment = '';
+            console.log(this.comments);
+        },
+
+        validateData: function () {
+            var MIN_LENGTH = 2;
+            var MAX_LENGTH_COMMENTS = 150;
+            var MAX_LENGTH_OTHERS = 100;
+            var values = this.getValues();
+
+            var LENGTH_CONDITION = function (value) {
+                return (value < MIN_LENGTH && value > MAX_LENGTH_OTHERS)
+            }
+
+            for (const value in this.values) {
+                if (this.values === 'dueDate' || this.values === 'reminder' || this.values === 'priority') {
+                    continue;
+                }
+                if (!(LENGTH_CONDITION(this.values[value]))) {
+                    alert('Invalid length. Length must be between 2 and 100 characters')
+                    return false;
+                }
+            }
+            return true;
+        },
+
+        validateAndCreate: function () {
+            var values = this.getValues();
+            if (this.validateData()) {
+                var newTask = new Task(values.taskName, values.projects, values.priority, values.dueDate, values.reminder, values.description, this.comments, false);
+                dataBase.addTask(newTask);
+            } else {
+                alert('Invalid new todo');
+            }
+        }
+    };
+    taskToAdd.init();
+    
+})();
+
+/*
+function Task (title, project, priority, dueData, reminder, description, comments, statement) {
+    this.id = null;
+    this.title = title;
+    this.project = project;
+    this.priority = priority;
+    this.dueData = dueData;
+    this.reminder = reminder;
+    this.description = description;
+    this.comments = comments;
+    this.statement = statement;
+    this.deleted = false;
+}
+*/
